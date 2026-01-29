ee140f7043508a71563b921b50468ef7b8c96904
r0se1ia <rslpetal@gmail.com>
2026-01-29 17:39:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
  * 請填入你的 GAS Web App URL（從 Apps Script「部署 → 管理部署」取得）
  */
 window.GUNDAM_TCG_CONFIG = {
-  GAS_URL: ''
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbwf2R6OPi13KBipjwd0mH_bN1CJz5Zf2VC6OeuP61ov8dQgs6xznKG1v0AgMUwK6gX0/exec'
 };
